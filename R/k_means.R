8fcf7fbf0c3abf899361ac2691c698be921f4eb2
ScottFB101 <113871715+ScottFB101@users.noreply.github.com>
2023-05-30 12:58:36 -0700

    Start of K means / package in general

---
+++ R/k_means.R
@@ -0,0 +1,28 @@
+#' Implements K means by hands
+#'
+#' @param dat A data set
+#' @param k The number of desired clusters
+#' @param pca Boolean value indicating whether or not to run Principal Component Analysis prior to K means
+#'
+#' @return
+#'
+#' @import dplyr
+#'
+#' @export
+
+k_means <- function(dat, k, pca = FALSE) {
+
+    random_obs <- sample(nrow(dat), size = k)
+
+    centers <- slice(dat, random_obs)
+
+    return(centers)
+
+}
+#Randomly select K points for K clusters
+
+#Measure distance between K1 and (K2 and K3) MSE, Euclidean Distance sqrt(x^2 + y^2)
+
+#Measure distance between all points in data and all K points. Assign to cluster that has smallest MSE
+
+#Assess quality of cluster by looking at variation in each cluster
