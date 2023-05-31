002a75e093b17cb7cc0eacbd3b168ca21790518e
ScottFB101 <113871715+ScottFB101@users.noreply.github.com>
2023-05-30 20:49:43 -0700

    Made repeat loop till cluster assignments match. Every iteration the centers that the clusters are based off of change to the mean of the cluster. Attempted creating total sum of squares function

--- R/k_means.R
+++ R/k_means.R
@@ -7,6 +7,7 @@
 #' @return
 #'
 #' @import dplyr
+#' @import stats
 #'
 #' @export
 
@@ -21,39 +22,84 @@ k_means <- function(dat, k, pca = FALSE) {
 
     #Randomly select K observations
     random_obs <- sample(num_obs, size = k)
+    #Create centers for reference
     centers <- slice(dat, random_obs)
 
     #Vectors of clusters
-    clusters <- c()
-    last_cluster <- c()
-    dist <- c()
+    clusters <- c(1)
+    last_clusters <- c(0)
+    smallest_distances <- c(0)
+    dist <- c(0)
+    iterations <- 0
 
-    #Loop to calculate Euclidean distance of each point from the randomly selected centers
-    for(i in 1:num_obs) {
+    #Center point of whole data set for Total Sum of Squares calculation
+    center_of_dat <- colMeans(dat)
 
-        center_and_point <- dat[i, ] %>% rbind(centers)
+    #Repeat until we end up on the same cluster
+    repeat {
 
-        dist <- dist(center_and_point, method = "euclidean", upper = FALSE)
+        #Loop to calculate Euclidean distance of each point from the randomly selected centers
+        for(i in 1:num_obs) {
 
-        closest_cluster <- dist[1:k] %>%
-            which.min()
+            #Attaching observation to data frame of the randomly chosen k-clusters
+            center_and_point <- dat[i, ] %>% rbind(centers)
 
-        clusters[i] <- closest_cluster
+            #Measuring Euclidean distance between observation and k-clusters
+            dist <- dist(center_and_point, method = "euclidean", upper = FALSE)
+
+            #Taking smallest distance out of the measurements for each k-clusters
+            closest_cluster <- dist[1:k] %>%
+                which.min()
+
+            #Pulling smallest distance for sum of squares
+            smallest_distances[i] <- dist[closest_cluster]
+
+            #Assigning cluster number to observation
+            clusters[i] <- closest_cluster
+
+        }
+
+        #Checking to make sure the cluster arraignment updated
+        if(all(last_clusters == clusters)) {
+            break
+        }
+
+        #Setting last cluster value
+        last_clusters <- clusters
+
+        #Finding new centers by taking mean of data points in each k-cluster, then removing cluster column
+        centers <- dat %>%
+            cbind(clusters) %>%
+            group_by(clusters) %>%
+            summarize_all(mean) %>%
+            select(-clusters)
+
+        #Counting number of iterations to get repeated k-means clustering
+        iterations <- iterations + 1
 
     }
 
-    #Check if cluster assignment has changed
-    stopifnot(clusters != last_cluster)
-
-    #Assigning new cluster assignments to variable so we can perform the check above
-    last_cluster <- clusters
-
     #Attaching cluster assignment to original data frame
     cluster_assignments <- dat %>%
-        cbind(clusters)
+        cbind(clusters) %>%
+        cbind(smallest_distances)
+
+    #Total Sum of Squares
+    TSS <- total_sum_squares(center_of_dat, dat)
 
     #Output cluster assignments, total sum of squares, at the minimum
-    return(cluster_assignments)
+    return(c(cluster_assignments, iterations))
 
 }
 
+
+#' Retrieves Total Sum of Squares
+#'
+#' @param center The center point of the data that all Euclidean distances will be calculated on
+#' @param dat A data set
+
+total_sum_squares <- function (center, dat) {
+
+
+
+}
