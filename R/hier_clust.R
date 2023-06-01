fb75d6a7d9458b297bf86ac848170634b192a57a
nberzinsCP <nberzins@calpoly.edu>
2023-05-31 21:49:49 -0700

    Update hier_clust.R
    changed edited to work proven 1 time through on data set, but not tested complete yet.

--- R/hier_clust.R
+++ R/hier_clust.R
@@ -1,9 +1,7 @@
 
 #' Agglomerative Hierarchical Clustering Method`
 #'
-#' @import
-#'
-#' @param
+#' @param data
 #'
 #' @return
 #'
@@ -14,17 +12,15 @@ hier_clust <- function(data, method = "euclidean") {
     avail_methods <- c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")
 
     if(!(method %in% avail_methods)) {
-    #STOP AND WARNING HERE
+
+        stop("Please put your distance calculation method in all lower case")
     }
+
+    n <- nrow(data)
 
     iterations <- 0 #will count the height of the dendrogram by adding 1 to each
 
     distances <- dist(data, method = method) #distance matrix
-
-    #while loop that checks the # of clusters isn't 2
-        #each loop checks for which two rows are most similar - includes cluster means
-            #can change later to not only compare based on means but also closest/furthest point in the cluster
-        #check for if either
 
     clusters <- list() #the list from which the clusters will be stored and manipulated
 
@@ -33,7 +29,7 @@ hier_clust <- function(data, method = "euclidean") {
     }
 
 
-    while(length(clusters) > 1) {
+    while(length(clusters) > 1) { #keep iterating until there is only one cluster left
 
         min_dist <- Inf
         merge_index <- NULL
@@ -45,38 +41,31 @@ hier_clust <- function(data, method = "euclidean") {
 
                 dist <- distances[cluster1, cluster2] #puts the indices of the clusters into the dist matrix
 
-                if(dist < min_dist) {
+                if(dist < min_dist) { #Sets the smallest distance between two points by finding the smallest distance value
                     min_dist <- dist
-                    merge_index <- c(i, j)
+                    merge_index <- c(i, j) #index in clusters
                 }
 
             }
 
         }
 
-        merged_cluster <- c(clusters[[merge_index[1]]], clusters[[merge_index[2]]])
-        clusters <- clusters[-merge_index]
-        clusters <- c(clusters, list(merged_cluster))
-
-        #Complete Linkage
-
-        #Single Linkage
+        merged_cluster <- c(clusters[[merge_index[1]]], clusters[[merge_index[2]]]) #create new cluster that adds in the min dist cluster pairing
+        clusters <- clusters[-merge_index] #deletes the indexes of both clusters in the pair
+        clusters <- c(clusters, list(merged_cluster)) #puts the new cluster back into the clusters list
 
         #Average Linkage
 
-        ######RESOURCES SAY TO DO distances <- distances[-merge_index[2], -merge_index[2]] HERE ######
-
-        for(i in length(clusters)) {
+        for(i in 1:length(clusters) - 1) { #-1 to account for the potential cluster value
 
             cluster_i <- clusters[[i]]
-            avg_dist <- sum(distances[merge_index[1], cluster_i], distances[merge_index[2], cluster_i]) / 2
-            distances[merge_index[1], cluster[i]] <- avg_dist
 
+            avg_dist <- sum(distances[merge_index[1], cluster_i], distances[merge_index[2], cluster_i]) / 2
+            distances[merge_index[1], cluster_i] <- avg_dist
+            distances[cluster_i, merge_index[1]] <- avg_dist
         }
 
-        distances[-merge_index[2], merge_index[2]]
-
-        #######BUT I FEEL LIKE ITS ONLY RIGHT TO MAKE THAT ROW/COL REMOVAL AFTER I'VE WORKED WITH THE VALUE IN THE FOR LOOP ABOVE
+        distances[-merge_index[2], -merge_index[2]]
 
     }
 
