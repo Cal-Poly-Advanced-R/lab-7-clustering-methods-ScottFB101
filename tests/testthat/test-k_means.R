8eef4d5fcf819525675b1f10425fcaa7cfd603fa
ryushida <28831863+ryushida@users.noreply.github.com>
2023-05-31 21:09:09 -0700

    Add test for plot_clustering

--- tests/testthat/test-k_means.R
+++ tests/testthat/test-k_means.R
@@ -1,3 +1,9 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("plot_clustering works", {
+    empty_clustering <- data.frame(matrix(ncol = 3, nrow = 0))
+    colnames(empty_clustering) <- c("Comp.1", "Comp.2", "clusters")
+    expect_error(plot_clusterings(empty_clustering))
+})
